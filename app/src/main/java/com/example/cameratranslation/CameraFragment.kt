1b3481d4c67d02cb98f30bfebd836d91a43e7a94
Alexander <alexael@ntnu.no>
2021-11-24 22:22:37 +0100

    Added XML layout android for camera view together with viewmodel for livedata

--- app/src/main/java/com/example/cameratranslation/CameraFragment.kt
+++ app/src/main/java/com/example/cameratranslation/CameraFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.example.cameratranslation.databinding.FragmentCameraBinding
+import com.example.cameratranslation.viewmodels.CameraViewModel
 
 /**
  * A simple [Fragment] subclass.
@@ -12,6 +15,8 @@ import android.view.ViewGroup
  * create an instance of this fragment.
  */
 class CameraFragment : Fragment() {
+    private val camViewModel: CameraViewModel by activityViewModels()
+    private var binding: FragmentCameraBinding? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +26,26 @@ class CameraFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val fragmentBinding = FragmentCameraBinding.inflate(inflater, container, false)
+        binding = fragmentBinding
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera, container, false)
+        //return inflater.inflate(R.layout.fragment_camera, container, false)
+        return fragmentBinding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding?.apply {
+            viewModel = camViewModel
+            lifecycleOwner = viewLifecycleOwner
+            cameraFragment = this@CameraFragment
+        }
+        //Set click listeners here
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+    }
 }
