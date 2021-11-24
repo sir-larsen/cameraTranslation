04bebe251538e92e18117cc9e3970f9654020d96
Alexander <alexael@ntnu.no>
2021-11-24 21:47:45 +0100

    Created camera fragment, phrase fragment and bootomnavigation component. Also linked the navigation component to the bottomnavigation

--- app/src/main/java/com/example/cameratranslation/MainActivity.kt
+++ app/src/main/java/com/example/cameratranslation/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.cameratranslation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController)
+
+        //Observers, if any
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
